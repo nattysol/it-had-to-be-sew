37f5d00173949f8bbb74e76a35b7150b94354139
Natural Solutions <kyle@naturalsolutions.llc>
2026-01-27 10:41:08 -0800

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'It Had To Be Sew',
+
+  projectId: 'lx1vm0bm',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
