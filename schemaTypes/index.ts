51359c8522ccbfa9c4b0786566a830733ef49aed
Natural Solutions <kyle@naturalsolutions.llc>
2026-01-27 20:44:42 -0800

    fix faq link

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -5,4 +5,4 @@ import order from './order' // <--- Import it
 import faq from './faq'
 
 // We export a simple array named 'schemaTypes'
-export const schemaTypes = [pantograph, material, settings, order]
+export const schemaTypes = [pantograph, material, settings, order, faq] // <--- Include 'order' here
