6957cb8fec26cb3b8785a7f113b69408ce580eee
Natural Solutions <kyle@naturalsolutions.llc>
2026-01-28 12:37:19 -0800

    groq w updated location

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,4 +1,4 @@
-import { client } from "@/sanity/lib/client";
+import { client } from "../../../sanity/lib/client";
 import OrderDashboard, { Order } from "./OrderDashboard";
 
 // 1. THE PRODUCTION QUERY
