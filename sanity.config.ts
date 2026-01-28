e59b6c545d04bc8a636b62fa2e707eac06068923
Natural Solutions <kyle@naturalsolutions.llc>
2026-01-28 15:41:10 -0800

    env

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,19 +1,27 @@
+'use client'
+
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
+ */
+
+import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+
+// Go to sanity/env.ts and check these variable names match yours
+import {apiVersion, dataset, projectId} from './sanity/env'
+import {schema} from './sanity/schema' // 👈 IMPORTING YOUR FILE
 
 export default defineConfig({
-  name: 'default',
-  title: 'It Had To Be Sew',
-
-  projectId: 'lx1vm0bm',
-  dataset: 'production',
-  basePath: '/studio', // <--- IMPORTANT: This must match your folder name
-
-  plugins: [structureTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
-  },
+  basePath: '/studio',
+  projectId,
+  dataset,
+  // Add and edit the content schema in the './sanity/schema' folder
+  schema, // 👈 USING YOUR FILE
+  plugins: [
+    structureTool(),
+    // Vision is a tool that lets you query your content with GROQ in the studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({defaultApiVersion: apiVersion}),
+  ],
 })
