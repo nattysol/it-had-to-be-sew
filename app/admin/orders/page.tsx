2f3ba8ab008d7009344298d2dd10ef01c14c5cd9
Natural Solutions <kyle@naturalsolutions.llc>
2026-01-28 12:21:58 -0800

    fix schema loc

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,4 +1,4 @@
-import { client } from "@/sanity/lib/client";
+import { client } from "../../../sanity/lib/client";
 import OrderDashboard, { Order } from "./OrderDashboard";
 
 // 1. DEFINE THE QUERY (Single Definition)
