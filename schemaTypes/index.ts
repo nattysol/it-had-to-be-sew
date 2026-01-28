bd98b72f8e89d8b0a6897cdb9db5723e8c640397
Natural Solutions <kyle@naturalsolutions.llc>
2026-01-28 15:07:14 -0800

    fix schema callout

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -3,6 +3,7 @@ import material from './material'
 import settings from './settings'
 import order from './order' // <--- Import it
 import faq from './faq'
+import pattern from './pattern'
 
 // We export a simple array named 'schemaTypes'
-export const schemaTypes = [pantograph, material, settings, order, faq] // <--- Include 'order' here
+export const schemaTypes = [pantograph, material, settings, order, faq, pattern] // <--- Include 'order' here
